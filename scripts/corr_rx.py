b206fb2a7cd3d5e96f4b967edc2b1ec1458dadf2
Paul Prozesky <paulp@ska.ac.za>
2014-01-02 13:16:03 +0200

    Prettified.

--- scripts/corr_rx.py
+++ scripts/corr_rx.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 """
  Capture utility for a relatively generic packetised correlator data output stream.
 
@@ -14,52 +16,81 @@ Revs:
 2010-11-26  JRM Added command-line option for autoscaling.
 """
 
-import numpy as np, spead, logging, sys, time, h5py, corr
+import numpy as np
+import spead
+import logging
+import sys
+import time
+import h5py
+import corr
 
 logging.basicConfig(level=logging.WARN)
-acc_scale=True
+acc_scale = True
 
 if __name__ == '__main__':
     from optparse import OptionParser
     p = OptionParser()
     p.set_usage('%prog [options] [CUSTOM_CONFIG_FILE]')
     p.set_description(__doc__)
-    p.add_option('-a', '--disable_autoscale', dest='acc_scale',action='store_false', default=True,
-        help='Do not autoscale the data by dividing down by the number of accumulations.  Default: Scale back by n_accs.')
-    p.add_option('-v', '--verbose', dest='verbose',action='store_true', default=False, 
-        help='Be verbose about errors.')
-    opts, args = p.parse_args(sys.argv[1:])
+    p.add_option(
+        '-a',
+        '--disable_autoscale',
+        dest='acc_scale',
+        action='store_false',
+        default=True,
+        help='Do not autoscale the data by dividing down by the number of accumulations.  Default: Scale back by n_accs.'
+            ,
+        )
+    p.add_option(
+        '-v',
+        '--verbose',
+        dest='verbose',
+        action='store_true',
+        default=False,
+        help='Be verbose about errors.',
+        )
+    (opts, args) = p.parse_args(sys.argv[1:])
 
-    if args==[]:
-        config_file=None
+    if args == []:
+        config_file = None
     else:
-        config_file=args[0]
-    acc_scale=opts.acc_scale  
-    verbose=opts.verbose  
+        config_file = args[0]
+    acc_scale = opts.acc_scale
+    verbose = opts.verbose
 
 print 'Parsing config file...',
 sys.stdout.flush()
-c=corr.corr_functions.Correlator(config_file=config_file,log_level=logging.DEBUG if verbose else logging.WARN, connect=False)
-config=c.config
+c = corr.corr_functions.Correlator(config_file=config_file,
+                                   log_level=(logging.DEBUG if verbose else logging.WARN),
+                                   connect=False)
+config = c.config
 print 'done.'
 
 data_port = config['rx_udp_port']
 sd_ip = config['sig_disp_ip_str']
 sd_port = config['sig_disp_port']
-mode=config['xeng_format']
+mode = config['xeng_format']
 
-filename=str(time.time()) + '.corr.h5'
+filename = str(time.time()) + '.corr.h5'
 
-print 'Initalising SPEAD transports for %s data...'%mode
-print "Data reception on port",data_port
-print "Sending Signal Display data to %s:%i."%(sd_ip,sd_port)
-print "Storing to file %s"%filename
+print 'Initalising SPEAD transports for %s data...' % mode
+print 'Data reception on port', data_port
+print 'Sending Signal Display data to %s:%i.' % (sd_ip, sd_port)
+print 'Storing to file %s' % filename
 
-crx=corr.rx.CorrRx(mode=mode,data_port=data_port,sd_ip=sd_ip,sd_port=sd_port,acc_scale=acc_scale,filename=filename,log_level=logging.DEBUG if verbose else logging.INFO)
+crx = corr.rx.CorrRx(
+    mode=mode,
+    data_port=data_port,
+    sd_ip=sd_ip,
+    sd_port=sd_port,
+    acc_scale=acc_scale,
+    filename=filename,
+    log_level=(logging.DEBUG if verbose else logging.INFO),
+    )
 try:
-    crx.daemon=True
+    crx.daemon = True
     crx.start()
-    while(crx.isAlive()):
+    while crx.isAlive():
         time.sleep(0.1)
     print 'RX process ended.'
     crx.join()
